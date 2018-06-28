3880818debfaa429725c4330d83a471d129ee0f4
Yoshinari Nomura <nom@cs.okayama-u.ac.jp>
2018-06-28 19:58:06 +0900

    Add command parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,99 @@
-// #[derive(Debug)]
-// enum Action<'a> {
-//     Append(Profile),
-//     Quit,
-//     Check,
-//     Print(i32),
-//     Read(&'a str),
-//     Write(&'a str),
-//     Find(&'a str),
-//     Sort(u8),
-//     Error(&'a str),
-// }
-//
-// impl<'a> Action<'a> {
-//     fn from_str<'b>(line: &'b str) -> Action<'a> {}
-// }
+use std::fmt::{self, Display};
+use std::num::ParseIntError;
+use std::str::FromStr;
 
 //////////////////
-// ProfOrCommand
+// Action
 //////////////////
 #[derive(Debug)]
-pub enum ProfOrCommand {
-    Profile(Profile),
-    Command(Command),
+pub enum Action<'a> {
+    Append(Profile),
+    Quit,
+    Count,
+    Print(i32),
+    Read(&'a str),
+    Write(&'a str),
+    Find(&'a str),
+    Sort(u8),
+    Unknown(&'a str),
+}
+
+//////////////////
+// Date
+//////////////////
+
+#[derive(Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub struct Date {
+    y: u32,
+    m: u8,
+    d: u8,
+}
+
+impl Date {
+    pub fn new(y: u32, m: u8, d: u8) -> Date {
+        Date { y, m, d }
+    }
+}
+
+// Display trait implements to_string
+impl Display for Date {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}-{}-{}", self.y, self.m, self.d)
+    }
+}
+
+// implement "2018-06-30".parse::<Date>
+impl FromStr for Date {
+    type Err = ParseIntError;
+
+    fn from_str(date_str: &str) -> Result<Self, Self::Err> {
+        let ymd: Vec<&str> = date_str.split("-").collect();
+        let y = ymd[0].parse::<u32>()?;
+        let m = ymd[1].parse::<u8>()?;
+        let d = ymd[2].parse::<u8>()?;
+        Ok(Date { y, m, d })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static TEST_DATE: Date = Date {
+        y: 2018,
+        m: 8,
+        d: 31,
+    };
+
+    #[test]
+    fn create_date_from_string() {
+        assert_eq!(Date::from_str("2018-8-31").unwrap(), TEST_DATE);
+    }
+
+    #[test]
+    fn format_date_string() {
+        assert_eq!("2018-8-31", format!("{}", TEST_DATE));
+    }
+
+    #[test]
+    fn date_is_string() {
+        assert_eq!("2018-8-31", format!("{}", TEST_DATE));
+    }
 }
 
 //////////////////
 // Command
 //////////////////
 #[derive(Debug)]
-pub struct Command {}
+pub struct Command {
+    name: String,
+}
 
 impl Command {
-    pub fn new() -> Command {
-        Command {}
+    pub fn new(name: &str) -> Command {
+        Command {
+            name: name.to_string(),
+        }
     }
 }
 
@@ -41,21 +102,21 @@ impl Command {
 //////////////////
 #[derive(Debug)]
 pub struct Profile {
-    id: String,
-    name: String,
-    birthday: String,
-    address: String,
-    description: String,
+    pub id: u32,
+    pub name: String,
+    pub birthday: Date,
+    pub home: String,
+    pub comment: String,
 }
 
 impl Profile {
     pub fn new(columns: Vec<&str>) -> Profile {
         Profile {
-            id: columns[0].to_string(),
+            id: columns[0].parse::<u32>().unwrap(),
             name: columns[1].to_string(),
-            birthday: columns[2].to_string(),
-            address: columns[3].to_string(),
-            description: columns[4].to_string(),
+            birthday: columns[2].parse::<Date>().unwrap(),
+            home: columns[3].to_string(),
+            comment: columns[4].to_string(),
         }
     }
 }
@@ -76,7 +137,7 @@ impl ProfileDB {
     }
 
     pub fn exec(&self, command: Command) -> () {
-        println!("exec command {:?}", command);
+        println!("exec command {}", command.name);
     }
 
     pub fn push(&mut self, profile: Profile) -> () {
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,15 @@ use db_sample::*;
 
 use std::io::{self, BufRead};
 
-// #[derive(Debug)]
-// enum Action<'a> {
-//     Append(Profile),
-//     Quit,
-//     Check,
-//     Print(i32),
-//     Read(&'a str),
-//     Write(&'a str),
-//     Find(&'a str),
-//     Sort(u8),
-//     Error(&'a str),
-// }
-//
-// impl<'a> Action<'a> {
-//     fn from_str<'b>(line: &'b str) -> Action<'a> {
-//     }
-// }
+// | コマンド | 意味                            | 備考                       |
+// |----------+---------------------------------+----------------------------|
+// | %Q       | 終了(Quit)                      |                            |
+// | %C       | 登録件数などの表示(Check)       |                            |
+// | %P n     | 先頭からn件表示 (Print)         | n=0: 全件, n<0: 後ろ -n 件 |
+// | %R file  | fileから読み込み(Read)          |                            |
+// | %W file  | fileへ書き出し(Write)           |                            |
+// | %F word  | wordを検索(Find)                | 結果を%Pと同じ形式で表示   |
+// | %S n     | データをn番目の項目で整列(Sort) | 表示はしない               |
 
 fn main() {
     let file = io::stdin();
@@ -27,22 +19,51 @@ fn main() {
 
     for line in file.lock().lines() {
         match parse_line(&line.unwrap()) {
-            Ok(ProfOrCommand::Profile(profile)) => profile_data_store.push(profile),
-            Ok(ProfOrCommand::Command(command)) => profile_data_store.exec(command),
-            Err(e) => println!("{}", e),
+            Action::Append(profile) => profile_data_store.push(profile),
+            Action::Quit => std::process::exit(0),
+            Action::Count => println!("Command: Count"),
+            Action::Print(start) => println!("Command: Print {}", start),
+            Action::Read(file) => println!("Command: Read {}", file),
+            Action::Write(file) => println!("Command: Write {}", file),
+            Action::Find(word) => println!("Command: Find {}", word),
+            Action::Sort(key) => println!("Command: Sort {}", key),
+            Action::Unknown(error) => println!("Error: {}", error),
+            // profile_data_store.exec(command),
         }
     }
 }
 
-fn parse_line(line: &str) -> Result<ProfOrCommand, &'static str> {
+fn parse_line(line: &str) -> Action {
     if line.starts_with("%") {
-        Ok(ProfOrCommand::Command(Command::new()))
+        parse_cmd(&line[1..])
     } else {
         if let Ok(profile) = parse_csv(line) {
-            Ok(ProfOrCommand::Profile(profile))
+            Action::Append(profile)
         } else {
-            Err("CSV parse error")
+            Action::Unknown("CSV parse error")
         }
+    }
+}
+
+fn parse_cmd(line: &str) -> Action {
+    let error = Action::Unknown("Unknown command");
+
+    if line.len() < 1 {
+        return error;
+    }
+
+    let command = &line[0..1];
+    let param = &line[1..].trim();
+
+    match command {
+        "Q" => Action::Quit,
+        "C" => Action::Count,
+        "P" => Action::Print(param.parse::<i32>().unwrap()),
+        "R" => Action::Read(param),
+        "W" => Action::Write(param),
+        "F" => Action::Find(param),
+        "S" => Action::Sort(param.parse::<u8>().unwrap()),
+        _ => error,
     }
 }
 
@@ -50,9 +71,26 @@ fn parse_csv(line: &str) -> Result<Profile, &'static str> {
     let columns: Vec<&str> = line.split(',').collect();
 
     // id, name, birthday, address, description
-    if columns.len() != 5 {
-        return Err("CSV format error: length != 5");
-    } else {
+    if columns.len() == 5 {
         Ok(Profile::new(columns))
+    } else {
+        Err("CSV format error: length != 5")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static TEST_CSV_STRING: &str = "1,nom,2018-6-28,okayama,hello!";
+
+    #[test]
+    fn parse_valid_csv() {
+        let p = parse_csv(TEST_CSV_STRING).unwrap();
+        assert_eq!(p.id, 1);
+        assert_eq!(p.name, "nom");
+        assert_eq!(p.birthday, Date::new(2018, 6, 28));
+        assert_eq!(p.home, "okayama");
+        assert_eq!(p.comment, "hello!");
     }
 }
